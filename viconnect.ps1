6f4a11a211088bf229bcac0901338a4afcd0e1ce
Attila Horváth <atiradeon86@gmail.com>
2023-12-29 21:16:41 +0100

    The first step ...

---
+++ viconnect.ps1
@@ -0,0 +1,22 @@
+#The first step
+
+#Create the local credential for login
+#New-VICredentialStoreItem -Host vcenter-server.bryan86.hu -User -Password
+
+#Ignore Invalid SSL Certification
+#Set-PowerCLIConfiguration -InvalidCertificateAction Ignore
+
+#Connect
+#Connect-VIServer -Server vcenter-server.bryan86.hu -Port 444 -User
+
+$server = "vcenter-server.bryan86.hu"
+$user = "administrator@bryan.local"
+
+function VIServer_login($server,$user,$port) {
+    Connect-VIServer -Server $server -Port 444 -User $user
+}
+
+VIServer_login($server,$user)
+
+#Test command
+Get-VMHost
